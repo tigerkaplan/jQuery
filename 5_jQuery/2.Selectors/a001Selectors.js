baced75caf64a10ec3515dbba3f603fd1f1b9040
tigerkaplane <husniyeerparun@gmail.com>
2024-03-21 15:21:16 +0000

    commit

---
+++ 5_jQuery/2.Selectors/a001Selectors.js
@@ -0,0 +1,19 @@
+
+// if you are using "external Js" document.ready 
+
+$("document").ready(function () {
+$("button").click(function () {
+    // $('p').hide();
+    // $('#idJquery').hide();
+    // $('.classJquery').hide();
+    // $("*").hide();
+    // $("ul li").hide();
+    // $("ul li:first").hide();
+    // $("ul li:first-child").hide();
+    // $("tr td").hide();
+    // $("tr:even").hide();
+    // $("tr:odd").hide();
+})
+});
+
+
